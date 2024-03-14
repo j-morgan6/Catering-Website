13ec11f152ca1a8d79ea6bd85d050a8418ff77e9
Derron Ward <derronward.dev@gmail.com>
2024-03-13 21:03:43 -0400

    Revert "Restructured Entry and Implemented Navbar"
    This reverts commit 55dd83abd49af2f307860e8977fe1bd937852f01.

--- Customer-Frontend/src/main.jsx
+++ Customer-Frontend/src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
 import './index.css'
 
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Home />} />
-            </Routes>
-        </Router>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
