27064bfe03e4e1856f83c68567894165cae961fd
estauss1 <estauss23@gmail.com>
2024-03-30 15:32:24 -0400

    initial

---
+++ Customer-Frontend/src/assets/tempMenuData/tempMenuData.js
@@ -0,0 +1,104 @@
+const categories = [
+    {
+        title: "Lunch",
+        img: "/src/assets/artisansandwiches.jpg"
+    },
+    {
+        title: "Beverages",
+        img: "src/assets/tempMenuData/beverages.jpg"
+    },
+    {
+        title: "Breakfast",
+        img: "src/assets/tempMenuData/breakfastsandwiches.jpg"
+    },
+    {
+        title: "À la Carte",
+        img: "src/assets/tempMenuData/lunchbox.jpg"
+    },
+    {
+        title: "Dessert",
+        img: "src/assets/tempMenuData/minipastries.jpg"
+    }
+]
+
+const menuItems = [
+    {
+        name: "Breakfast combo",
+        description: "Baked Good or Muffin + Yogurt Parfait or Fruit Salad",
+        price: "10.15",
+        Category: "Breakfast",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Breakfast Platter",
+        description: "Assortment of muffins, sweet and savoury baked goods",
+        price: "5.75",
+        Category: "Breakfast",
+        ImageURL: "",
+        dietary: ""
+    },
+    {
+        name: "Mini Baked Good Platter",
+        description: "2 per person",
+        price: "5.75",
+        Category: "Breakfast",
+        ImageURL: "",
+        dietary: []
+    },
+    {
+        name: "",
+        description: "",
+        price: "",
+        Category: "",
+        ImageURL: "",
+        dietary: []
+    },
+    {
+        name: "",
+        description: "",
+        price: "",
+        Category: "",
+        ImageURL: "",
+        dietary: []
+    },
+    {
+        name: "",
+        description: "",
+        price: "",
+        Category: "",
+        ImageURL: "",
+        dietary: []
+    },
+    {
+        name: "",
+        description: "",
+        price: "",
+        Category: "",
+        ImageURL: "",
+        dietary: []
+    },
+]
+
+const options = [
+    {
+        menuItem: "",
+        name: "",
+        dietary: ""
+    },
+    {
+        menuItem: "",
+        name: "",
+        dietary: ""
+    },
+    {
+        menuItem: "",
+        name: "",
+        dietary: ""
+    },
+    {
+        menuItem: "",
+        name: "",
+        dietary: ""
+    },
+]
