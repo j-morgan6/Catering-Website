6234e05a5be600ac28bc8603f014b4461a046904
estauss1 <estauss23@gmail.com>
2024-03-31 06:47:17 -0400

    display menu items and some styling

--- Customer-Frontend/src/components/MenuComponents.jsx
+++ Customer-Frontend/src/components/MenuComponents.jsx
@@ -1,15 +1,31 @@
 
-function CategoryCard({category}){
+function CategoryCard({category, clickHandler}){
     return(
-        <div className="card catCard">
+        <div className="card catCard" onClick={() => clickHandler(category.title)}>
             {(category.img !== "") ? (
-                <img src={category.img}/>
+                <img className="menuImg" src={category.img}/>
             ) : (
-                <img src={"src/assets/tempMenuData/placeholder.jpg"}/>
+                <img className="menuImg" src={"src/assets/tempMenuData/placeholder.jpg"}/>
             )}
-            <h2>{category.title}</h2>
+            <h2 className="cardTitle">{category.title}</h2>
         </div>
     );
 }
 
-export {CategoryCard};
+function MenuItemCard({item, clickHandler}){
+    return(
+        <div className="card itemCard" onClick={() => clickHandler(item.name)}>
+            {(item.imageURL !== "") ? (
+                <img className="menuImg" src={item.imageURL}/>
+            ) : (
+                <img className="menuImg" src="src/assets/tempMenuData/placeholder.jpg"/>
+            )}
+            <div className="cardBar">
+                {item.vegetarian && <img className="vegLogo" src="src/assets/tempMenuData/veglogo.png"/>}
+                <h2 className="cardTitle">{item.name}</h2>
+            </div>
+        </div>
+    );
+}
+
+export {CategoryCard, MenuItemCard};
