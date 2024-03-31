c1b3e1da6c18c8d6d6810ef43a6f3d798ffecfb0
estauss1 <estauss23@gmail.com>
2024-03-31 03:30:05 -0400

    temp data and render categories

--- Customer-Frontend/src/pages/Menu.jsx
+++ Customer-Frontend/src/pages/Menu.jsx
@@ -1,9 +1,18 @@
 import {React, useState} from "react";
+import { categories, menuItems } from "../assets/tempMenuData/tempMenuData";
+import { CategoryCard } from "../components/MenuComponents";
 
 export default function Menu(){
     const [view, setView] = useState("categories")
 
     return(
-        <div>test</div>
+    <div>
+        <h1>Menu</h1>
+        {view == "categories" &&
+            categories.map((category) => 
+                <CategoryCard category={category}/>
+            )
+        }
+    </div>
     );
 }
