22bcb982ed2e2473bb2760c42733754a0d40eb48
estauss1 <estauss23@gmail.com>
2024-03-31 03:30:05 -0400

    temp data and render categories

--- Customer-Frontend/src/assets/tempMenuData/tempMenuData.js
+++ Customer-Frontend/src/assets/tempMenuData/tempMenuData.js
@@ -1,7 +1,7 @@
 const categories = [
     {
         title: "Lunch",
-        img: "/src/assets/artisansandwiches.jpg"
+        img: "src/assets/tempMenuData/artisansandwiches.jpg"
     },
     {
         title: "Beverages",
@@ -36,7 +36,7 @@ const menuItems = [
         price: "5.75",
         Category: "Breakfast",
         ImageURL: "",
-        dietary: ""
+        vegetarian: false
     },
     {
         name: "Mini Baked Good Platter",
@@ -44,61 +44,164 @@ const menuItems = [
         price: "5.75",
         Category: "Breakfast",
         ImageURL: "",
-        dietary: []
+        vegetarian: false
     },
     {
-        name: "",
+        name: "Sandwich Platter",
         description: "",
-        price: "",
-        Category: "",
+        price: "13.35",
+        Category: "Lunch",
         ImageURL: "",
-        dietary: []
+        vegetarian: false
     },
     {
-        name: "",
+        name: "Side Salads",
         description: "",
-        price: "",
-        Category: "",
+        price: "6.25",
+        Category: "Lunch",
         ImageURL: "",
-        dietary: []
+        vegetarian: true
     },
     {
-        name: "",
-        description: "",
-        price: "",
-        Category: "",
+        name: "Coffee Thermos (7 to 10 people)",
+        description: "Rich dark or medium roast",
+        price: "27.95",
+        Category: "Beverages",
         ImageURL: "",
-        dietary: []
+        vegetarian: false
     },
     {
-        name: "",
+        name: "Spring Water",
         description: "",
-        price: "",
-        Category: "",
+        price: "2.75",
+        Category: "Beverages",
         ImageURL: "",
-        dietary: []
+        vegetarian: false
     },
-]
+    {
+        name: "Fruit and Cheese Platters",
+        description: "",
+        price: "6.55",
+        Category: "À la Carte",
+        ImageURL: "",
+        vegetarian: true
+    },
+    {
+        name: "Dessert Platter",
+        description: "",
+        price: "4.95",
+        Category: "Desserts",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Cookie Platter",
+        description: "",
+        price: "3.50",
+        Category: "Desserts",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Mini croissant",
+        description: "",
+        price: "3.95",
+        Category: "Mini Baked Good Platter",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Mini chocolate croissant",
+        description: "",
+        price: "3.95",
+        Category: "Mini Baked Good Platter",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Bocconcini and tomato",
+        description: "",
+        price: "13.35",
+        Category: "Sandwich Platter",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Roast Beef",
+        description: "",
+        price: "13.35",
+        Category: "Sandwich Platter",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Beet and goat cheese",
+        description: "",
+        price: "6.25",
+        Category: "Side Salads",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Greek",
+        description: "",
+        price: "6.25",
+        Category: "Side Salads",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Fresh fruit",
+        description: "",
+        price: "6.55",
+        Category: "Fruit and Cheese Platters",
+        ImageURL: "",
+        vegetarian: false
+    },
+    {
+        name: "Fruit and cheese",
+        description: "",
+        price: "7.15",
+        Category: "Fruit and Cheese Platters",
+        ImageURL: "",
+        vegetarian: false
+    },
 
+]
+/*
 const options = [
     {
-        menuItem: "",
-        name: "",
+        menuItem: "Mini Baked Good Platter",
+        name: "Mini croissant",
         dietary: ""
+        vegetarian: false
     },
     {
-        menuItem: "",
-        name: "",
-        dietary: ""
+        menuItem: "Mini Baked Good Platter",
+        name: "Mini chocolate croissant",
+        vegetarian: false
     },
     {
-        menuItem: "",
-        name: "",
-        dietary: ""
+        menuItem: "Sandwich Platter",
+        name: "Bocconcini and tomato",
+        vegetarian: true
     },
     {
-        menuItem: "",
-        name: "",
-        dietary: ""
+        menuItem: "Sandwich Platter",
+        name: "Roast Beef",
+        vegetarian: false
+    },
+    {
+        menuItem: "Side Salads",
+        name: "Beet and goat cheese",
+        vegetarian: true
+    },
+    {
+        menuItem: "Side Salads",
+        name: "Beet and goat cheese",
+        vegetarian: true
     },
 ]
+*/
+
+export {categories, menuItems};
