98bf33d88e3a6645d272190f9575576e9ced9621
estauss1 <estauss23@gmail.com>
2024-03-31 06:47:17 -0400

    display menu items and some styling

--- Customer-Frontend/src/pages/Menu.jsx
+++ Customer-Frontend/src/pages/Menu.jsx
@@ -1,17 +1,61 @@
 import {React, useState} from "react";
 import { categories, menuItems } from "../assets/tempMenuData/tempMenuData";
-import { CategoryCard } from "../components/MenuComponents";
+import { CategoryCard, MenuItemCard } from "../components/MenuComponents";
+import './Menu.css'
 
 export default function Menu(){
     const [view, setView] = useState("categories")
+    const [allItems, setAllItems] = useState(menuItems)
+    const [shownItems, setShownItems] = useState([])
+    const [shownOptions, setShownOptions] = useState([])
+
+    function HandleCategoryClick(categoryTitle){
+        //set shownItems to items of category clicked
+        const newShownItems = allItems.filter((item) =>
+            item.category === categoryTitle
+        )
+        setShownItems(newShownItems)
+
+        setView("items")
+    }
+
+    function HandleItemClicked(itemName){
+        const newShownOptions = allItems.filter((item) =>
+            item.category === itemName
+        )
+        setShownOptions(newShownOptions)
+
+        setView("expanded")
+    }
 
     return(
-    <div>
+    <div className="menu">
         <h1>Menu</h1>
         {view == "categories" &&
+            <div className="section categories">
+            {
             categories.map((category) => 
-                <CategoryCard category={category}/>
+                <CategoryCard 
+                    key={category.title} 
+                    category={category} 
+                    clickHandler={HandleCategoryClick}
+                />
             )
+            }
+            </div>
+        }
+        {view == "items" &&
+            <div className="section items">
+            {
+            shownItems.map((item) =>
+                <MenuItemCard 
+                    key={item.name}
+                    item={item}
+                    clickHandler={HandleItemClicked}
+                />
+            )
+            }
+            </div>
         }
     </div>
     );
