b171126c945ec9702dd730af037d1d78979bfc40
estauss1 <estauss23@gmail.com>
2024-04-02 05:16:34 -0400

    expanded menu view styling and features

--- Customer-Frontend/src/components/MenuComponents.jsx
+++ Customer-Frontend/src/components/MenuComponents.jsx
@@ -30,6 +30,90 @@ function MenuItemCard({item, clickHandler}){
 }
 
 function ExpandedItemSection({expandedItem, options}){
+    const [quantities, setQuantities] = useState((options.length !== 0) ? options.map((option) => {
+        return({
+            optionName: option.name,
+            quantity: 0
+        });
+    }) : (
+        [{
+            optionName: expandedItem.name,
+            quantity: 0
+        }]
+    ))
+
+    let total = 0 
+    quantities.forEach((quantity) => {
+        if(quantity.quantity > 0)
+            total += quantity.quantity * GetPrice(quantity.optionName)
+    })
+    const totalStr = total.toFixed(2)
+
+    function PlusPressed(item){
+        const itemIndex = quantities.findIndex((quantity) =>
+            quantity.optionName === item.name
+        )
+        if(itemIndex !== -1){
+            const newQuantities = quantities.map((quantity) => {
+                return {...quantity}
+            })
+            if(newQuantities[itemIndex].quantity < 30)
+                ++newQuantities[itemIndex].quantity
+            setQuantities(newQuantities)
+        }else
+            console.log("Tried to update quantity of nonexistant item")
+    }
+
+    function SubPressed(item){
+        const itemIndex = quantities.findIndex((quantity) =>
+            quantity.optionName === item.name
+        )
+        if(itemIndex !== -1){
+            const newQuantities = quantities.map((quantity) => {
+                return {...quantity}
+            })
+            if(newQuantities[itemIndex].quantity > 0)
+                --newQuantities[itemIndex].quantity
+            setQuantities(newQuantities)
+        }else
+            console.log("Tried to update quantity of nonexistant item")
+    }
+
+    function InputChanged(e, item){
+        const itemIndex = quantities.findIndex((quantity) =>
+            quantity.optionName === item.name
+        )
+        if(itemIndex !== -1){
+            const newQuantities = quantities.map((quantity) => {
+                return {...quantity}
+            })
+            newQuantities[itemIndex].quantity = parseInt(e.target.value)
+            setQuantities(newQuantities)
+        }else
+            console.log("Tried to update quantity of nonexistant item")
+    }
+
+    function GetQuantity(item){
+        const itemQuant = quantities.find((quantity) => 
+            quantity.optionName === item.name
+        )
+        return itemQuant.quantity
+    }
+
+    function GetPrice(itemName){
+        if(expandedItem.name == itemName)
+            return expandedItem.price
+        else{
+            const matchingItem = options.find((option) =>
+                option.name === itemName
+            )
+            if(matchingItem !== undefined)
+                return matchingItem.price
+            else
+                console.log("couldnt find price of item")
+        }
+    }
+
     return(
         <div className="expanded">
             {(expandedItem.imageURL !== "") ? (
@@ -38,43 +122,47 @@ function ExpandedItemSection({expandedItem, options}){
                 <img className="expImg" src="src/assets/tempMenuData/placeholder.jpg"/>
             )}
             <div className="infoSec">
-                <h2 className="expTitle">{expandedItem.name}</h2>
-                {expandedItem.description !== "" && <p>{expandedItem.description}</p>}
+                <h2 className="expTitle">
+                    {(options.length !== 0) ? expandedItem.name 
+                        : expandedItem.name + " $" + expandedItem.price}
+                </h2>
+                {expandedItem.description !== "" && <p className="itemDesc">{expandedItem.description}</p>}
                 {  
-                (options.length === 0) ?(
-                    <h3>{expandedItem.price}</h3>
+                (options.length === 0) ? (
+                    <div className="quantContainer">
+                        <QuantityBar
+                            item={expandedItem}
+                            GetQuantity={GetQuantity}
+                            PlusPressed={PlusPressed}
+                            SubPressed={SubPressed}
+                            InputChanged={InputChanged}
+                        />
+                    </div>
                 ) : (
                     <div className="optionContainer">
                         {options.map((option) =>
-                            <Option key={option.name} item={option}/>
+                            <Option 
+                                key={option.name} 
+                                item={option}
+                                GetQuantity={GetQuantity}
+                                PlusPressed={PlusPressed}
+                                SubPressed={SubPressed}
+                                InputChanged={InputChanged}
+                            />
                         )}
                     </div>
                 )
                 }
-                <h3></h3>
-                <button className="addToCart">Add to cart</button>
+                <div className="infoBottomBar">
+                    <h3>Total: ${totalStr}</h3>
+                    <button className="addToCart">Add to cart</button>
+                </div>
             </div>
         </div>
     );
 }
 
-function Option({item}){
-    const [quantity, setQuantity] = useState(0)
-
-    function PlusPressed(){
-        if(quantity < 30) 
-            setQuantity(quantity + 1)
-    }
-
-    function SubPressed(){
-        if(quantity > 0)
-            setQuantity(quantity - 1)
-    }
-
-    function InputChanged(e){
-        setQuantity(parseInt(e.target.value))
-    }
-
+function Option({item, GetQuantity, PlusPressed, SubPressed, InputChanged}){
     return(
         <div className="option">
             <div className="optionLeft">
@@ -82,20 +170,32 @@ function Option({item}){
                 {item.vegetarian && 
                     <img className="vegLogo" src="src/assets/tempMenuData/veglogo.png"/>}
             </div>
-            <div className="quantityBar">
-                <button className="quantButton" onClick={SubPressed}>-</button>
-                <input 
-                    className="quantInput"
-                    type="number" 
-                    min="0"
-                    max="30"
-                    value={quantity}
-                    onChange={InputChanged}
-                />
-                <button className="quantButton" onClick={PlusPressed}>+</button>
-            </div>
+            <QuantityBar 
+                item={item}
+                GetQuantity={GetQuantity}
+                SubPressed={SubPressed}
+                PlusPressed={PlusPressed}
+                InputChanged={InputChanged}
+            />
         </div>
     );
+}
+
+function QuantityBar({item, GetQuantity, SubPressed, PlusPressed, InputChanged}){
+    return(
+        <div className="quantityBar">
+            <button className="quantButton" onClick={() => SubPressed(item)}>-</button>
+            <input 
+                className="quantInput"
+                type="number" 
+                min="0"
+                max="30"
+                value={GetQuantity(item)}
+                onChange={(e) => InputChanged(e, item)}
+            />
+            <button className="quantButton" onClick={() => PlusPressed(item)}>+</button>
+        </div>
+    )
 }
 
 export {CategoryCard, MenuItemCard, ExpandedItemSection};
