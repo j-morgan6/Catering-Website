693aa236eed094bb8bdcb554ec5ee6d238d00051
dsantoli <dsantoli10@outlook.com>
2024-04-10 20:30:17 -0400

    Pushing initial admin-frontend setup

--- Admin-Frontend/src/App.jsx
+++ Admin-Frontend/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { UserContext } from './hooks/useUser';
+import axios from 'axios';
+import { useAccessToken } from './hooks/useAccessToken';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import Dashboard from './pages/Dashboard'; // Make sure to import the Dashboard component
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [user, setUser] = useState(null);
+    const login = async (accessToken) => {
+        const apiURI = `http://${import.meta.env.VITE_API_DOMAIN}:${import.meta.env.VITE_API_PORT}`;
+        try {
+            const response = await axios.get(`${apiURI}/user`, {
+                headers: {
+                    "Authorization": `Bearer ${accessToken}`
+                }
+            });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            setUser(response.data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    const logout = () => setUser(null);
+
+    useEffect(() => {
+        const getUser = async () => {
+            const accessToken = await useAccessToken();
+            if (accessToken) await login(accessToken);
+        };
+        getUser();
+    }, []);
+
+    return (
+        <UserContext.Provider value={{ user, login, logout }}>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Navigate replace to={user ? "/dashboard" : "/auth"} />} />
+                    <Route path='/auth' element={<Auth />} />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate replace to="/auth" />} />
+                </Routes>
+            </Router>
+        </UserContext.Provider>
+    );
 }
 
-export default App
+export default App;
---
+++ Admin-Frontend/src/pages/Auth.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useUser } from '../hooks/useUser';
+import BreadmanGIF from '../assets/big_bread.gif';
+import './Auth.css';
+
+function Auth() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [formError, setFormError] = useState('');
+
+    const { user, login } = useUser();
+    const navigate = useNavigate();
+
+    // If the user is logged in, redirect to home
+    useEffect(() => {
+        if (user) navigate('/');
+    }, [user, navigate]);
+
+    async function handleLogin(event) {
+        event.preventDefault();
+        const credentials = { email, password };
+        const apiURI = `http://${import.meta.env.VITE_API_DOMAIN}:${import.meta.env.VITE_API_PORT}`;
+
+        try {
+            const response = await axios.post(`${apiURI}/auth/login`, credentials, {
+                headers: { "Content-Type": "application/json" },
+                withCredentials: true
+            });
+
+            await login(response.data.accessToken);
+            navigate('/Dashboard');
+        } catch (err) {
+            if (err.response && err.response.status === 401) {
+                setFormError("Incorrect username or password");
+            } else {
+                setFormError("We encountered an error logging you in. Please try again later.");
+            }
+        }
+    }
+
+    return (
+        <div id='auth-page'>
+            <div className='auth-container'>
+                <h1>Welcome Back!</h1>
+                <img src={BreadmanGIF} alt="Breadman" />
+            </div>
+            <div className='auth-container'>
+                <form onSubmit={handleLogin} className='auth-form'>
+                    <input className='input-text' type="email" name='email' value={email} onChange={e => setEmail(e.target.value)} placeholder='Email*' required />
+                    <input className='input-text' type="password" name='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Password*' required />
+                    <input type="submit" value="Log In" />
+                </form>
+                {formError && <p id='login-error'>{formError}</p>}
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
