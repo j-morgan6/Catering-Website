28f60e0b0670d150590554377b4b5ef53c496af1
estauss1 <estauss23@gmail.com>
2024-04-11 21:06:19 -0400

    cart basic functionality

--- Customer-Frontend/src/pages/Cart.jsx
+++ Customer-Frontend/src/pages/Cart.jsx
@@ -1,8 +1,11 @@
 import {React, useState, useEffect} from "react";
 import Cookies from "js-cookie";
+import { CartItemSection } from "../components/CartComponents";
 
 export default function Cart(){
     const [orderItems, setOrderItems] = useState([])
+
+    console.log(orderItems)
 
     //get cart from cookies on mount
     useEffect(() => {
@@ -14,11 +17,41 @@ export default function Cart(){
         }
     }, [])
 
+    //update cart cookie when orderItems changes
+    useEffect(() => {
+        if(orderItems.length > 0){
+            let cartItemsStr = JSON.stringify(orderItems)
+            Cookies.set('cart', cartItemsStr, {sameSite: 'None'})
+            console.log(`SET cart cookies to:\n ${cartItemsStr}`)
+        }
+    }, [orderItems])
+
+    function ChangeQuantity(updatedOrderItem){
+        let newOrderItems = JSON.parse(JSON.stringify(orderItems)) //deepcopy
+        const i = newOrderItems.findIndex((orderItem) =>
+            orderItem.name === updatedOrderItem.name
+        )
+        if(i !== -1){
+            newOrderItems[i].quantity = updatedOrderItem.quantity
+            setOrderItems(newOrderItems)
+        } else
+            console.log("couldn't change quantity of orderItem in ChangeQuantity", updatedOrderItem)
+    }
+
+    function RemoveItem(itemToRemove){
+        const newOrderItems = orderItems.filter((item) =>
+            item.name !== itemToRemove.name
+        )
+        setOrderItems(newOrderItems)
+    }
+
     return(
     <div className="cart">
-        <div className="items">
-
-        </div>
+        <CartItemSection
+            orderItems={orderItems}
+            ChangeQuantity={ChangeQuantity}
+            RemoveItem={RemoveItem}
+        />
         <div className="form">
 
         </div>
