12e46cc7da5c944fff1eca161c2199dde59b47fd
dsantoli <dsantoli10@outlook.com>
2024-04-14 15:36:32 -0400

    Updated table

--- Admin-Frontend/src/components/Navbar.jsx
+++ Admin-Frontend/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useUser } from '../hooks/useUser';
 import Logo from '../assets/APD-Logo-Circular.png';
 import './Navbar.css';
 
 function Navbar() {
     const { user } = useUser(); // Removed 'login' as it's not used here.
+    const navigate = useNavigate();
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    const handleLogout = () => {
+    };
 
     return (
         <div className='navbar'>
@@ -17,14 +23,9 @@ function Navbar() {
                     <img src={Logo} alt="Navbar Logo" />
                 </Link>
                 <div id='navbar-links'>
-                    {/* Uncomment or add other navigation links as needed */}
-                    {/* <Link className='navbar-link' to='/menu'>Menu</Link> */}
-                    {/* <Link className='navbar-link' to='/cart'>Cart</Link> */}
-                    {/* <Link className='navbar-link' to='/contact'>Contact</Link> */}
-                    {user && (
-                        <Link className='navbar-link' to='/profile'>{user.FirstName} {user.LastName}</Link>
-                    )}
-                    {/* Removed the Login link for unauthenticated users */}
+                    {user && currentPath !== '/auth' && (<Link className='navbar-link' to='/dashboard'>Orders</Link>)}
+                    {user && (<Link className='navbar-link' to='/profile'>{user.FirstName} {user.LastName}</Link>)}
+                    {user && (<button onClick={handleLogout} className='navbar-link'>Logout</button>)}
                 </div>
             </div>
         </div>
