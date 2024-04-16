ed745749b90c89afa536276dd1f151afb0194bce
j-morgan6 <jsm10242000@gmail.com>
2024-04-15 20:44:29 -0400

    home page

--- Customer-Frontend/src/pages/Home.jsx
+++ Customer-Frontend/src/pages/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import './Home.css';
+import myGif from '../assets/Generique.gif';
 
 function Home() {
   return (
     <div id='Home'>
-        <h1>Make changes to Home.jsx and save to see changes</h1>
+      <div className='left-half'>
+        <div className='home-content'>
+          <h1>Our Secret</h1>
+          <p>We carefully select top quality ingredients, while prioritizing local sourcing and promoting the local economy.</p>
+        </div>
+      </div>
+      <div className='right-half'>
+        <img src={myGif} alt="My GIF" className ="gif-image" style={{ maxWidth: '100%', height: 'auto' }} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
