c520551216928031c3ca3d36c71c6a53dfc17aa7
Derron Ward <derronward.dev@gmail.com>
2024-04-19 13:18:52 -0400

    order query

--- Catering-API/src/routes/orders.js
+++ Catering-API/src/routes/orders.js
@@ -67,8 +67,12 @@ router.get('/', (req, res) => {
     \`Order\`.*,
     (SELECT SUM(MenuItem.Price * OrderItem.Quantity) FROM MenuItem JOIN OrderItem ON MenuItem.ID = OrderItem.MenuItemID WHERE OrderItem.OrderID = \`Order\`.ID) as Total,
     Customer.FirstName, Customer.LastName, Customer.Company,
+    CustomerAuth.Email, CustomerAuth.Phone,
     Store.Name as StoreName, Store.StreetNumber, Store.StreetName, Store.City, Store.Province, Store.PostalCode
-    FROM (\`Order\` JOIN Store ON \`Order\`.StoreID = Store.ID) JOIN Customer ON \`Order\`.CustomerID = Customer.ID
+    FROM (\`Order\`
+        JOIN Store ON \`Order\`.StoreID = Store.ID)
+        JOIN Customer ON \`Order\`.CustomerID = Customer.ID
+        JOIN CustomerAuthentication AS CustomerAuth ON Customer.ID = CustomerAuth.CustomerID 
     ${filterStr ? `WHERE ${filterStr} ` : ''}
     ${orderBy}
     `)
@@ -76,7 +80,7 @@ router.get('/', (req, res) => {
     const guestOrderStmt = Database.prepare(`SELECT
     \`Order\`.*,
     (SELECT SUM(MenuItem.Price * OrderItem.Quantity) FROM MenuItem JOIN OrderItem ON MenuItem.ID = OrderItem.MenuItemID WHERE OrderItem.OrderID = \`Order\`.ID) as Total,
-    Guest.FirstName, Guest.LastName, Guest.Company,
+    Guest.FirstName, Guest.LastName, Guest.Company, Guest.Email, Guest.Phone,
     Store.Name as StoreName, Store.StreetNumber, Store.StreetName, Store.City, Store.Province, Store.PostalCode
     FROM (\`Order\` JOIN Store ON \`Order\`.StoreID = Store.ID) JOIN Guest ON \`Order\`.GuestID = Guest.ID
     ${filterStr ? `WHERE ${filterStr} ` : ''}
